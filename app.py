a70d9c8fe4011eee17451b4588a173f4606763b2
Филатов Никита Антонович <s22270031@unn.ru>
2025-09-18 11:07:17 +0300

    tests5

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(_name_)
 def hello():
     return 'Hi!'
 @app.route('/health')
-def health:
+def health():
     return 'ok',200
 if _name_ == '_main_':
     app.run(host='0.0.0.0',port = 5000)
-     
